230ae4b2cb123c63bf1e2ecb7333830144cc1b2f
drishtantR1508 <drishtant.rai.min19@itbhu.ac.in>
2020-10-27 18:33:02 +0530

    Modification #1 in models.py

--- my_django_stuff/shopping_project/shop_app/models.py
+++ my_django_stuff/shopping_project/shop_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product = models.CharField(max_length=200)
+    description = models.CharField(max_length=1200)
